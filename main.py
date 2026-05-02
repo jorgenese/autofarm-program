5d104700fe4e463f4f4ffec110b441e409ba5918
jorgenese <103547137+jorgenese@users.noreply.github.com>
2026-05-01 22:41:20 -0800

    Create main.py for drone function calls
    Initialize main.py to call core functions for drone control.

---
+++ main.py
@@ -0,0 +1,8 @@
+# This will be the main platform that will call the functions from the other locations for easy and clean code structure. 
+# ****Still work in progress****
+
+from core_functions import *
+
+x = get_pos_x()
+y = get_pos_y()
+center_drone(x, y)
